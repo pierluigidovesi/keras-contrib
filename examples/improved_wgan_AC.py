0c12f5156d4a2e1ca99d43345153ee128f77781e
pierluigidovesi <pierluigidovesi@gmail.com>
2018-05-13 20:10:04 +0200

    bazza

--- examples/improved_wgan_AC.py
+++ examples/improved_wgan_AC.py
@@ -262,6 +262,7 @@ args = parser.parse_args()
 # First we load the image data, reshape it and normalize it to the range [-1, 1]
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 X_train = np.concatenate((X_train, X_test), axis=0)
+y_train = np.concatenate((y_train, y_test), axis=0)
 
 if K.image_data_format() == 'channels_first':
     X_train = X_train.reshape((X_train.shape[0], 1, X_train.shape[1], X_train.shape[2]))
